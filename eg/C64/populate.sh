1c753810816ce9a96e4c6e92196b93b6bb99fae6
Chris Pressey <cpressey@catseye.tc>
2014-08-27 08:24:10 +0100

    Put some programs on the C64 system image.

---
+++ eg/C64/populate.sh
@@ -0,0 +1,4 @@
+cp distrepos/bubble-escape/bin/* device8/
+cp distrepos/c64-demo-depot/petulant/petulant.prg device8/petulant
+cp distrepos/c64-demo-depot/petulant/ribos.prg device8/ribos
+cp distrepos/disksumo/bin/disksumo.prg device8/disksumo
