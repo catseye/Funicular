74bda6b3e2aa6a6d4ddc79f30fe8ea70a5d6cc1a
Chris Pressey <cpressey@catseye.tc>
2017-04-13 17:01:07 +0100

    It'll be something like this, I wager.

--- eg/NetBSD/skel/populate-shelf.sh
+++ eg/NetBSD/skel/populate-shelf.sh
@@ -20,5 +20,13 @@ for PROJECT in "
     shelf_build $PROJECT
 done
 
-# extract_project: echo $line | awk '{split($0,a,"@"); print a[1]}'
-# extract_tag: echo $line | awk '{split($0,a,"@"); print a[2]}'
+while read -r line; do
+    PROJECT=`echo $line | awk '{split($0,a,"@"); print a[1]}'`
+    TAG=`echo $line | awk '{split($0,a,"@"); print a[2]}'`
+    tar zxvf /cdrom/$PROJECT.tar.gz
+    cd $PROJECT
+    if [ "X$TAG" != X ]; then
+        git checkout $TAG
+    fi
+    shelf_build $PROJECT
+done < catseye.catalog
