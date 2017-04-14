ffe4019a94af00dd49d8257fc625e042bc550bb6
Chris Pressey <cpressey@catseye.tc>
2017-04-14 09:59:19 +0100

    Continue to refine away from toolshelf, towards shelf.

--- eg/NetBSD/skel/populate-shelf.sh
+++ eg/NetBSD/skel/populate-shelf.sh
@@ -6,27 +6,26 @@ cd /home/user
 mkdir -p catseye
 cd catseye
 
-for PROJECT in "
-  make-3.81
-  nano-2.2.6
-  mercurial-1.9.3
-  perl-5.18.2
-  hugs98-plus-Sep2006
-  otp_src_R16B03-1
-  chicken-4.8.0.5
-  yasm-1.2.0
-"; do
+while read -r line; do
+    PROJECT=`echo $line | awk '{split($0,a,"@"); print a[1]}'`
+    TAG=`echo $line | awk '{split($0,a,"@"); print a[2]}'`
     tar zxvf /cdrom/$PROJECT.tar.gz
+    if [ "X$TAG" != X ]; then
+        cd $PROJECT
+        git checkout $TAG
+        cd ..
+    fi
     shelf_build $PROJECT
-done
+done < /cdrom/infrastructure.catalog
 
 while read -r line; do
     PROJECT=`echo $line | awk '{split($0,a,"@"); print a[1]}'`
     TAG=`echo $line | awk '{split($0,a,"@"); print a[2]}'`
     tar zxvf /cdrom/$PROJECT.tar.gz
-    cd $PROJECT
     if [ "X$TAG" != X ]; then
+        cd $PROJECT
         git checkout $TAG
+        cd ..
     fi
     shelf_build $PROJECT
-done < catseye.catalog
+done < /cdrom/catseye.catalog
