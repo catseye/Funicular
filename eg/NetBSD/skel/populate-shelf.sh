00c78e1543aec6c644899cfe8e9009ec3d735273
Chris Pressey <cpressey@catseye.tc>
2017-04-19 08:56:52 +0100

    Working on the populate/build script, rather ad-hoc & experimental.

--- eg/NetBSD/skel/populate-shelf.sh
+++ eg/NetBSD/skel/populate-shelf.sh
@@ -2,30 +2,33 @@
 
 . /home/user/.shelf/shelf.sh
 
+shelf_populate() {
+    src_dir="$1"
+    while read -r line; do
+        project=`echo $line | awk '{split($0,a,"@"); print a[1]}'`
+        tag=`echo $line | awk '{split($0,a,"@"); print a[2]}'`
+        tar zxvf $src_dir/$project.tar.gz
+        #if [ "X$tag" != X ]; then
+        #    cd $project && git checkout $tag && cd ..
+        #fi
+        #shelf_build $project
+        #shelf_link $project
+    done
+}
+
 cd /home/user
 mkdir -p catseye
 cd catseye
 
-while read -r line; do
-    PROJECT=`echo $line | awk '{split($0,a,"@"); print a[1]}'`
-    TAG=`echo $line | awk '{split($0,a,"@"); print a[2]}'`
-    tar zxvf /cdrom/$PROJECT.tar.gz
-    if [ "X$TAG" != X ]; then
-        cd $PROJECT
-        git checkout $TAG
-        cd ..
-    fi
-    shelf_build $PROJECT
-done < /cdrom/infrastructure.catalog
+shelf_populate /cdrom/ < /cdrom/infrastructure.catalog || exit 1
+shelf_populate /cdrom/ < /cdrom/catseye.catalog || exit 1
 
-while read -r line; do
-    PROJECT=`echo $line | awk '{split($0,a,"@"); print a[1]}'`
-    TAG=`echo $line | awk '{split($0,a,"@"); print a[2]}'`
-    tar zxvf /cdrom/$PROJECT.tar.gz
-    if [ "X$TAG" != X ]; then
-        cd $PROJECT
-        git checkout $TAG
-        cd ..
-    fi
-    shelf_build $PROJECT
-done < /cdrom/catseye.catalog
+( cd make-3.81 && ./configure --prefix=`pwd` && make && make install ) || exit 1
+( cd nano-2.2.6 && ./configure --prefix=`pwd` && make && make install ) || exit 1
+( cd Python-2.7.6 && ./configure --prefix=`pwd` && make && make install ) || exit 1
+( cd perl-5.18.2 && C_INCLUDE_PATH="" ./Configure -des -Dprefix=`pwd`/install && C_INCLUDE_PATH="" make && make install ) || exit 1
+
+# shelf_build  hugs98-plus-Sep2006
+# shelf_build  otp_src_R16B03-1
+# shelf_build  chicken-4.8.0.5
+# shelf_build  yasm-1.2.0
